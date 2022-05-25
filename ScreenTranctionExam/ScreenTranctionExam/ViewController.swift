6aa0194a3ecadec0580dfcd720d007dd045e5de1
gnuoynawh <bizxhy@gmail.com>
2022-05-25 16:47:20 +0900

    [Basic] Ch02. ViewController 생명주기

--- ScreenTranctionExam/ScreenTranctionExam/ViewController.swift
+++ ScreenTranctionExam/ScreenTranctionExam/ViewController.swift
@@ -11,8 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("ViewController 뷰가 로드되었다")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewController 뷰가 나타날 것이다")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ViewController 뷰가 나타났다")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("ViewController 뷰가 사라질 것이다")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("ViewController 뷰가 사라졌다")
+    }
+    
+
+    
 
     @IBAction func tapCodePushButton(_ sender: Any) {
         guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePushViewController") else { return }
@@ -26,5 +49,7 @@ class ViewController: UIViewController {
         viewController1.modalPresentationStyle = .fullScreen
         self.navigationController?.present(viewController1, animated: true)
     }
+    
+    
 }
 
