c8d5b8de563b77600d22f7936b69f46f1dae8537
gnuoynawh <bizxhy@gmail.com>
2022-05-25 17:09:04 +0900

    [Basic] Ch02. 화면간 데이터 전달

--- ScreenTranctionExam/ScreenTranctionExam/CodePresentViewController.swift
+++ ScreenTranctionExam/ScreenTranctionExam/CodePresentViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
+protocol SendDataDelegate: AnyObject {
+    func sendData(name: String)
+}
+
 class CodePresentViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    var name: String?
+    weak var delegate: SendDataDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
     }
+    
     @IBAction func tapBackButton(_ sender: Any) {
+        self.delegate?.sendData(name: "TEST")
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
 }
--- ScreenTranctionExam/ScreenTranctionExam/ViewController.swift
+++ ScreenTranctionExam/ScreenTranctionExam/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SendDataDelegate {
 
+    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ViewController 뷰가 로드되었다")
@@ -38,18 +39,36 @@ class ViewController: UIViewController {
     
 
     @IBAction func tapCodePushButton(_ sender: Any) {
-        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePushViewController") else { return }
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePushViewController")
+            as? CodePushViewController
+            else { return }
         
+        viewController.name = "Gunter"
         self.navigationController?.pushViewController(viewController, animated: true)
     }
     
     @IBAction func tapCodePresentButton(_ sender: Any) {
-        guard let viewController1 = self.storyboard?.instantiateViewController(withIdentifier: "CodePresentViewController") else { return }
+        guard let viewController1 = self.storyboard?.instantiateViewController(withIdentifier: "CodePresentViewController")
+            as? CodePresentViewController
+            else { return }
         
         viewController1.modalPresentationStyle = .fullScreen
+        viewController1.name = "Gunter"
+        viewController1.delegate = self
         self.navigationController?.present(viewController1, animated: true)
     }
     
+    func sendData(name: String) {
+        self.nameLabel.text = name
+        self.nameLabel.sizeToFit()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let viewController = segue.destination as? SeguePushViewController {
+            viewController.name = "Test"
+        }
+            
+    }
     
 }
 
