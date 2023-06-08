489380eda71e2ce38a902da6ba9b02c167ca0343
gnuoynawh <>
2023-06-08 14:25:27 +0900

    Basic 챕터3 비밀다이어리, 챕터4 계산기 - UI 그리기

--- BasicChapter04/app/src/main/java/com/gnuoynawh/basicchapter04/MainActivity.kt
+++ BasicChapter04/app/src/main/java/com/gnuoynawh/basicchapter04/MainActivity.kt
@@ -2,10 +2,16 @@ package com.gnuoynawh.basicchapter04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun clearButtonClicked(view: View) {}
+    fun buttonClicked(view: View) {}
+    fun historyButtonClicked(view: View) {}
+    fun resultButtonClicked(view: View) {}
 }
