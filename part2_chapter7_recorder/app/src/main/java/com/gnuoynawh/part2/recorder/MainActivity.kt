4e9a23d9d7443c40017ce269cf34c3e38ac6bab0
gnuoynawh <bizxhy@gmail.com>
2023-09-08 23:43:44 +0900

    [Basic] 챕터7 Recorder앱 작업중

--- part2_chapter7_recorder/app/src/main/java/com/gnuoynawh/part2/recorder/MainActivity.kt
+++ part2_chapter7_recorder/app/src/main/java/com/gnuoynawh/part2/recorder/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.gnuoynawh.part2.recorder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordButton)
+    }
+
+    private val requestPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private val state = State.BEFORE_RECORDING
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestAudioPermission()
+        initViews()
+    }
+
+    private fun initViews() {
+        recordButton.updateIconWithState(state)
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requestPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted = requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.first() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPermissionGranted) {
+            finish()
+        }
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
---
+++ part2_chapter7_recorder/app/src/main/java/com/gnuoynawh/part2/recorder/State.kt
@@ -0,0 +1,8 @@
+package com.gnuoynawh.part2.recorder
+
+enum class State {
+    BEFORE_RECORDING,
+    ON_RECORDING,
+    ATFER_RECORDING,
+    ON_PLAYING
+}
